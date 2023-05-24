3b2f1edd53d9266546c6fd889ca9cae5e4ea9ad8
andi lovetto <andilovetto@gmail.com>
2023-05-24 09:41:04 -0600

    feat: Write Course class and methods

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,24 @@
 class Course
 
+  attr_reader :name, :capacity, :students
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @full = false
+  end
+
+  def full?
+    if @capacity == @students.count
+      @full = true
+    else
+      @full
+    end
+  end
+
+  def enroll(student)
+    @students.push(student)
+  end
+
 end
