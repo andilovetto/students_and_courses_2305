53d9d4001715ff1c28ea52861580af35b8eeabdc
andi lovetto <andilovetto@gmail.com>
2023-05-24 09:21:38 -0600

    feat: Write Student class and grade method

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student
+
+  attr_reader :name, :age, :scores
+
+  def initialize(details)
+    @name = details[:name]
+    @age = details[:age]
+    @scores = []
+  end
  
+  def log_score(score)
+    @scores.push(score)
+  end
+
+  def grade
+    scores.sum / scores.size.to_f  end
 end
